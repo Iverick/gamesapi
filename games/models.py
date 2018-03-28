7015c3c6cecd597688b4761d2631da1252c79d0a
Pawel Goloblia <iverick091@gmail.com>
2018-03-28 19:15:13 +0300

    Added Game model to the games application.

--- games/models.py
+++ games/models.py
@@ -1,3 +1,16 @@
+# django imports
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    '''
+    Game.models
+    '''
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+    release_date = models.DateTimeField()
+    game_category = models.CharField(max_length=200, blank=True, default='')
+    played = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
